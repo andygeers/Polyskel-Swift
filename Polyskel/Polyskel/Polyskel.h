bc204349cc17b87a00f73d1f8c77c352555cb9f9
Andy Geers <andy@hubbub.co.uk>
2019-11-22 21:34:18 +0000

    Build project

---
+++ Polyskel/Polyskel/Polyskel.h
@@ -0,0 +1,19 @@
+//
+//  Polyskel.h
+//  Polyskel
+//
+//  Created by Andy Geers on 22/11/2019.
+//  Copyright © 2019 Andy Geers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Polyskel.
+FOUNDATION_EXPORT double PolyskelVersionNumber;
+
+//! Project version string for Polyskel.
+FOUNDATION_EXPORT const unsigned char PolyskelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Polyskel/PublicHeader.h>
+
+
---
+++ Polyskel/PolyskelTests/PolyskelTests.swift
@@ -0,0 +1,34 @@
+//
+//  PolyskelTests.swift
+//  PolyskelTests
+//
+//  Created by Andy Geers on 22/11/2019.
+//  Copyright © 2019 Andy Geers. All rights reserved.
+//
+
+import XCTest
+@testable import Polyskel
+
+class PolyskelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
