55a3ec93ffc16f94d04f09200df68d9682de2bb0
Andy Geers <andy@hubbub.co.uk>
2019-11-29 13:47:49 +0000

    Add tests for the Euclid extensions

---
+++ Example/Tests/LinesTests.swift
@@ -0,0 +1,61 @@
+//
+//  LinesTests.swift
+//  GeometryScriptTests
+//
+//  Created by Andy Geers on 20/11/2019.
+//  Copyright © 2019 Andy Geers. All rights reserved.
+//
+
+import Polyskel
+import Euclid
+import XCTest
+
+class LinesTests: XCTestCase {
+    
+    // MARK: Vector distance
+    
+    func testDistanceFromPointSimple() {
+        let l = Line(point: Vector(0, 0, 0), direction: Vector(1, 0, 0))
+        let p = Vector(15, 2, 0)
+        XCTAssertEqual(l.distance(to: p), 2)
+    }
+    
+    func testDistanceFromPointHarder() {
+        let l = Line(point: Vector(0, 0, 0), direction: Vector(1, 0, 0))
+        let p = Vector(15, 2, 3)
+        XCTAssertEqual(l.distance(to: p), (2*2 + 3*3).squareRoot())
+    }
+    
+    func testLineIntersectionXY() {
+        let l1 = Line(point: Vector(1, 0, 3), direction: Vector(1, 0, 0))
+        let l2 = Line(point: Vector(0, 1, 3), direction: Vector(0, -1, 0))
+        
+        let intersection = l1.intersection(with: l2)
+        XCTAssertNotNil(intersection)
+        if (intersection != nil) {
+            XCTAssertEqual(intersection, Vector(0, 0, 3).quantized())
+        }
+    }
+    
+    func testLineIntersectionXZ() {
+        let l1 = Line(point: Vector(1, 3, 0), direction: Vector(1, 0, 0))
+        let l2 = Line(point: Vector(0, 3, 1), direction: Vector(0, 0, -1))
+        
+        let intersection = l1.intersection(with: l2)
+        XCTAssertNotNil(intersection)
+        if (intersection != nil) {
+            XCTAssertEqual(intersection, Vector(0, 3, 0).quantized())
+        }
+    }
+    
+    func testLineIntersectionYZ() {
+        let l1 = Line(point: Vector(3, 1, 0), direction: Vector(0, 1, 0))
+        let l2 = Line(point: Vector(3, 0, 1), direction: Vector(0, 0, -1))
+        
+        let intersection = l1.intersection(with: l2)
+        XCTAssertNotNil(intersection)
+        if (intersection != nil) {
+            XCTAssertEqual(intersection, Vector(3, 0, 0).quantized())
+        }
+    }
+}
---
+++ Example/Tests/VectorTests.swift
@@ -0,0 +1,55 @@
+//
+//  VectorTests.swift
+//  GeometryScriptTests
+//
+//  Created by Andy Geers on 20/11/2019.
+//  Copyright © 2019 Andy Geers. All rights reserved.
+//
+
+import Polyskel
+@testable import Euclid
+import XCTest
+
+class VectorTests: XCTestCase {
+    
+    // MARK: Vector length
+    
+    func testAxisAlignedLength() {
+        let vector = Vector(1, 0, 0)
+        XCTAssertEqual(vector.length, 1)
+    }
+    
+    func testAngledLength() {
+        let vector = Vector(2, 5, 3)
+        let length = (2.0 * 2.0 + 5.0 * 5.0 + 3.0 * 3.0).squareRoot()
+        XCTAssertEqual(vector.length, length)
+    }
+    
+    // MARK: Angle with vector
+    
+    func testRightAngle() {
+        let vector1 = Vector(1, 0, 0)
+        let vector2 = Vector(0, 1, 0)
+        XCTAssertEqual(vector1.angleWith(vector2), Double.pi / 2.0)
+    }
+    
+    func testNonNormalizedAngle() {
+        let vector1 = Vector(10, 0, 0)
+        let vector2 = Vector(-10, 0, 0)
+        XCTAssertEqual(vector1.angleWith(vector2), Double.pi)
+    }
+    
+    // MARK: Angle with plane
+    
+    func testRightAngleWithPlane() {
+        let vector1 = Vector(1, 0, 0)
+        let plane = Plane(normal: vector1, pointOnPlane: Vector.zero)
+        XCTAssertEqual(vector1.angleWith(plane: plane!), Double.pi / 2.0)
+    }
+    
+    func testNonNormalizedAngleWithPlane() {
+        let vector1 = Vector(7, 0, 0)
+        let plane = Plane(normal: vector1, pointOnPlane: Vector.zero)
+        XCTAssertEqual(vector1.angleWith(plane: plane!), Double.pi / 2.0)
+    }
+}
